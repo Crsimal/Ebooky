bfdaa8538cd1cf539916f36ce133caf26ee54093
Cristian <Cristian@Cristian-PC>
2017-05-20 19:24:47 +0200

    Acceso restringido a la página de votar para los usuarios que ya han votado ese dia

--- protected/controllers/AccionesController.php
+++ protected/controllers/AccionesController.php
@@ -46,9 +46,9 @@ class AccionesController extends Controller {
         } else {
             $acciones = Users::model()->with('acciones')->findByPk($usuario->id_usuario);
             $accion = $acciones->acciones;
-            if ($accion->ha_escrito == 1){
+            if ($accion->ha_escrito == 1) {
                 $this->render('yaEscrito');
-            }else{
+            } else {
                 if (isset($_POST['parrafo'])) {
                     $criteria = new CDbCriteria;
                     $criteria->select = 'max(id_parrafo) AS id_parrafo';
@@ -64,48 +64,55 @@ class AccionesController extends Controller {
                     $model->publicado = 0;
 
                     //Cuando el usuario escribe se modifica la tabla de acciones indicandolo
-                
+
                     $accion->ha_escrito = 1;
-                    $accion->save();                 
+                    $accion->save();
                     $model->save();
-                }    
+                    $this->redirect(array('landing/index'));
+                }else{
                 $this->render('escribir', array('model' => $model));
+                }
             }
         }
-        }
-
-        public
-
-        function actionVotar() {
-
-            $model = new Parrafos;
-
-            if (isset($_POST['votado'])) {
-                //sumamos un voto
-                $parrafo = $model->findByAttributes(array("id_parrafo" => $_POST['votado']));
-                $sumaVoto = $parrafo->votos + 1;
-                $parrafo->votos = $sumaVoto;
-                $parrafo->save();
-
-                //modificamos la tabla de acciones de usuario indicando que ya ha votado
-                $usuarios = new Users();
-                $usuario = $usuarios->findByAttributes(array("nickname" => Yii::app()->user->name));
-                $acciones = Users::model()->with('acciones')->findByPk($usuario->id_usuario);
-                $accion = $acciones->acciones;
-                $accion->ha_votado = 1;
-                $accion->save();
-            }
-
-            if (Yii::app()->user->isGuest) {
-                $this->render('accesoRestringido');
-            } else {
-                $usuarios = new Users();
-                $modelo = $usuarios->findByAttributes(array("nickname" => Yii::app()->user->name));
-
-
-                $this->render('votar', array('model' => $model));
-            }
-        }
-
     }
-    
+
+    public function actionVotar() {
+
+        $model = new Parrafos;
+
+        $usuarios = new Users();
+        $usuario = $usuarios->findByAttributes(array("nickname" => Yii::app()->user->name));
+        
+        if (Yii::app()->user->isGuest) {
+            $this->render('accesoRestringido');
+        } else {
+        
+        $acciones = Users::model()->with('acciones')->findByPk($usuario->id_usuario);
+        $accion = $acciones->acciones;
+        if ($accion->ha_votado == 1) {
+            $this->render('yaEscrito');
+        }else{
+
+        if (isset($_POST['votado'])) {
+            //sumamos un voto
+            $parrafo = $model->findByAttributes(array("id_parrafo" => $_POST['votado']));
+            $sumaVoto = $parrafo->votos + 1;
+            $parrafo->votos = $sumaVoto;
+            $parrafo->save();
+
+            //modificamos la tabla de acciones de usuario indicando que ya ha votado
+            $usuarios = new Users();
+            $usuario = $usuarios->findByAttributes(array("nickname" => Yii::app()->user->name));
+            $acciones = Users::model()->with('acciones')->findByPk($usuario->id_usuario);
+            $accion = $acciones->acciones;
+            $accion->ha_votado = 1;
+            $accion->save();
+            $this->redirect(array('landing/index'));
+        }
+        else{
+            $this->render('votar', array('model' => $model));
+        }
+        }
+    }
+    }
+}
